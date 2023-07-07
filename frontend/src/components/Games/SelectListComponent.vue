e748abda062e6dbc92ea7e503b99404a38f01e34
Teresa Fernández Coro <uo263728@uniovi.es>
2023-07-07 11:51:32 +0200

    feat: popup impl

--- frontend/src/components/Games/SelectListComponent.vue
+++ frontend/src/components/Games/SelectListComponent.vue
@@ -6,18 +6,26 @@
           <font-awesome-icon icon="close" />
         </button>
         <h3 class="title">Select list</h3>
-        <p>Select one of your lists to add the game.</p>
       </div>
-      <form @submit.prevent="submitForm">
-        <div class="lists-container" v-for="list in gameLists" :key="list.id">
-          <label><input type="radio" name="r" checked> list.status</label>
-          <label><input type="radio" name="r"> list.status</label>
-          <label><input type="radio" name="r"> list.status</label>
-        </div>
-        <div class="button-container">
-          <button type="submit" class="select-button">Add</button>
-        </div>
-      </form>
+      <div v-if="added" class="added-gameLists">
+        <p>Game added to your list.</p>
+      </div>
+      <div v-else>
+      <div v-if="!gameLists || gameLists.length === 0" class="empty-gameLists">
+        <p>First, create your own list to add games.</p>
+      </div>
+      <div v-else class="content-gameLists">
+        <p>Select one of your lists to add the game.</p>
+        <form @submit.prevent="submitForm">
+          <div class="lists-container" v-for="list in gameLists" :key="list.id">
+            <label><input type="radio" name="r" :value="list" v-model="selectedList"> {{ list.status }}</label>
+          </div>
+          <div class="button-container">
+            <button type="submit" class="add-button">Add</button>
+          </div>
+        </form>
+      </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,18 +46,28 @@
     },
     data() {
       return {
-        gameLists: []
+        gameLists: [],
+        selectedList: null,
+        added: false
       };
     },
     async mounted() {
       try {
-        /* Obtengo las listas de juegos del usuario en sesión */
+        // GET the users game lists
         const userId = sessionStorage.getItem('userId');
-        const response = await fetch(`http://localhost:8080/api/game-lists/user/${userId}`);
+        const response = await fetch(`http://localhost:8080/api/game-lists/user/${userId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + sessionStorage.getItem('jwtoken'),
+          },
+        });
         if (!response.ok)
-          throw new Error('Unable to fetch users game lists data');
-        const data = await response.json();
-        this.gameLists = await data;
+          console.log('An error response was received');
+        else {
+          const data = await response.json();
+          this.gameLists = await data;
+        }
       } catch(error) {
         console.error(error);
       }
@@ -60,8 +78,30 @@
       },
       async submitForm() {
         try {
-          /* Añadir el game a la lista seleccionada */
-          this.$router.push('/my-lists');
+          // Add the game to the selected list
+          if (this.selectedList === null) {
+            console.log('User did not select a list');
+            return;
+          }
+          const gameId = this.$route.params.gameId;
+          const listId = this.selectedList.id;
+          const requestBody = {...this.selectedList, status: this.selectedList.status};
+          const response = await fetch(`http://localhost:8080/api/game-lists/${listId}/add/${gameId}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + sessionStorage.getItem('jwtoken'),
+            },
+            body: JSON.stringify(requestBody)
+          });
+          if (!response.ok) {
+            this.added = false;
+            const errorResponseText = await response.text();
+            this.error = errorResponseText || 'An error occurred during editing.';
+          }
+          else {
+            this.added = true;
+          }
         } catch(error) {
           console.error(error);
         }
@@ -71,6 +111,7 @@
 </script>
   
 <style scoped>
+
 .popup-container {
   position: fixed;
   top: 0;
@@ -83,17 +124,79 @@
   background-color: rgba(0, 0, 0, 0.5);
 }
 
-.popup-container form {
-  max-width: 400px;
-  width: 100%;
+.popup-content {
+  background-color: #fff;
+  padding: 0rem 2rem 2rem 2rem;
+  border-radius: 4px;
+}
+
+.header-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0 auto;
 }
 
-.popup-container input {
+.close-button {
+  border: none;
+  background-color: rgb(252, 9, 76);
+  color: #fff;
+  cursor: pointer;
+  height: 38px;
+  width: 48px;
+  border-radius: 10px;
+  font-family: 'Poppins', sans-serif;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.close-button svg {
+  width: 24px;
+  height: 24px;
+}
+
+.title {
+  width: 200px;
+  height: 30px;
+  color: rgb(252, 9, 76);
+  font-family: 'Poppins', sans-serif;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  margin-left: 10rem;
+  margin: 0;
+}
+
+.content-gameLists p {
+  color: rgb(34, 35, 35, 0.5);
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: -1rem;
+}
+
+.lists-container {
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.lists-container label {
+  font-family: Poppins;
+  font-size: 18px;
+  margin: 15px;
+  margin-top: 25px;
+  cursor: pointer;
+}
+
+.lists-container input {
   --s: 1em;             /* control the size */
   --c: rgb(252, 9, 76); /* the active color */
 
@@ -112,89 +215,25 @@
   transition: .3s;
 }
 
-.popup-container input:checked {
+.lists-container input:checked {
   border-color: var(--c);
   background-size: 100% 100%;
-}
-
-.popup-container label {
-  font-family: Poppins;
-  font-size: 18px;
-  display: inline-flex;
-  align-items: center;
-  margin: 15px;
-  margin-top: 25px;
-  cursor: pointer;
-}
-
-.header-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.header-container p {
-  color: rgb(34, 35, 35, 0.5);
-  font-family: Poppins;
-  font-size: 16px;
-  font-weight: 500;
-  text-align: center;
-  margin-bottom: -1rem;
-}
-
-.title {
-  width: 200px;
-  height: 30px;
-  color: rgb(252, 9, 76);
-  font-family: 'Poppins', sans-serif;
-  font-size: 24px;
-  font-weight: 600;
-  text-align: center;
-  margin-left: 10rem;
-  margin: 0;
 }
 
 .button-container {
   display: flex;
   justify-content: center;
-  gap: 1rem;
   align-items: center;
+  margin-top: 1rem;
 }
 
-.close-button {
-  border: none;
-  background-color: rgb(252, 9, 76);
-  color: #fff;
-  cursor: pointer;
-  height: 38px;
-  width: 48px;
-  border-radius: 10px;
-  font-family: 'Poppins', sans-serif;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-
-.close-button svg {
-  width: 24px;
-  height: 24px;
-}
-
-.select-button {
+.add-button {
   border: none;
   background-color: rgb(252, 9, 76);
   color: #fff;
   cursor: pointer;
   height: 48px;
   border-radius: 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5rem;
   padding: 16px;
   font-family: 'Poppins', sans-serif;
   text-align: center;
@@ -203,14 +242,13 @@
   line-height: 18px;
 }
 
-.popup-content {
-  background-color: #fff;
-  padding: 0rem 2rem 2rem 2rem;
-  border-radius: 4px;
-}
-
-.popup-content button {
-  margin-top: 1rem;
+.empty-gameLists p, .added-gameLists p {
+  color: rgb(34, 35, 35, 0.5);
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: -1rem;
 }
 
 </style>
