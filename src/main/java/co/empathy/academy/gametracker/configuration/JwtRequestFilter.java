e5718db2750ed582d7a0caa9dd1b9693919bad85
joscacgom <jose9_negro@hotmail.com>
2023-07-17 12:54:34 +0200

    fix: elastic dependencies error

--- src/main/java/co/empathy/academy/gametracker/configuration/JwtRequestFilter.java
+++ src/main/java/co/empathy/academy/gametracker/configuration/JwtRequestFilter.java
@@ -2,7 +2,6 @@ package co.empathy.academy.gametracker.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
--- src/main/java/co/empathy/academy/gametracker/configuration/SecurityConfig.java
+++ src/main/java/co/empathy/academy/gametracker/configuration/SecurityConfig.java
@@ -2,15 +2,12 @@ package co.empathy.academy.gametracker.configuration;
 
 import co.empathy.academy.gametracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
